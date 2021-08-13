41fdddcc840c5b9359b4f1e294412b36ad7eeaf5
firefly <apatel698@gmail.com>
2021-08-13 11:08:02 -0400

    SwitchStatement modified

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         System.out.println("Hello Anar!");
 
     }//main
+
 }//class
 
 
---
+++ src/SwitchStatement.java
@@ -0,0 +1,8 @@
+public class SwitchStatement{
+    public void Switch(int value){
+        if(value == 1){
+            System.out.println("True");
+        }
+    }
+
+}//class
