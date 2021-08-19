3ef6e0aff2dc775abf33f5c856c337473a8fb195
firefly <apatel698@gmail.com>
2021-08-19 15:32:06 -0400

    new method SpeedConverter

--- src/GameOver.java
+++ src/GameOver.java
@@ -1,19 +1,49 @@
 public class GameOver {
     public static void GameScore() {
 
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
-    }
-    public static void calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
+       } //GameScore method
+    public static int calculateScore(boolean gameOver,int score, int levelCompleted, int bonus) {
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
-        }
-    }
-}
+            return finalScore;
+        } return -1;
+    }// calculateScore method
 
-        /* above code snippet is better version of below: */
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " managed to get into position "
+                + position + " on the high score table.");
+
+    }// displayHighScorePosition method
+
+    public static int calculateHighScorePosition(int score) {
+        int position; // this is needed to initiate int before assigning the value
+        if (score > 1000) {
+            position = 1; //return 1;
+        } else if (score <= 1000) {
+            position = 2; // return 2;
+        } else if (score <= 500) {
+            position = 3; //return 3
+        } else {
+            position = 4; //return 4
+        } return position; // this return is necessary
+    } // calculateHighScorePosition method
+    }// class
+
+
+
+
+
+
+
+
+
+
+
+
+        /* above code snippet is cleaner version of below: */
 //        boolean gameOver = true;
 //        int score = 5000;
 //        int levelCompleted = 5;
--- src/Hello.java
+++ src/Hello.java
@@ -7,6 +7,15 @@ public class Hello {
         Operator.NewChal();
         GameOver.GameScore();
         GameOver.calculateScore(true, 100,1,2000);
+        int highScorePosition = GameOver.calculateHighScorePosition(600);
+            GameOver.displayHighScorePosition("Jay",highScorePosition);
+
+
+        SpeedConverter.printConversion(1.5); // should return value 1
+        SpeedConverter.printConversion(10.25); // should return value 6
+        SpeedConverter.printConversion(-5.6); // should return value -1
+        SpeedConverter.printConversion(25.42); // should return value 16
+        SpeedConverter.printConversion(75.114); // should return value 47
     }//main
 
 
